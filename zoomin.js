1aab77651749fb9ade4aea8365229c51bc6d531b
Abdulrahman-AQ505 <abdulrahman.attallah0@gmail.com>
2025-05-05 11:35:26 +0300

    Add files via upload

--- zoomin.js
+++ zoomin.js
@@ -1,11 +1,10 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > window.innerHeight - 100) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+
+
+  
+ 
+  
+
+
 
 
   
